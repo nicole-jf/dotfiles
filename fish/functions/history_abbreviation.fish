895b3e9373d22651e6021b790a3629b65010cf6c
example@example.com <example@example.com>
2025-09-02 13:23:12 -0300

    removing grammars, they probably shouldn't be tracked by git, unsure how or why there were being tracked by git, probably my fault.

--- fish/functions/history_abbreviation.fish
+++ fish/functions/history_abbreviation.fish
@@ -1,5 +1,5 @@
 function history_abbreviation
-    set --local indexes ($HOME/.local/bin/parser decode $argv)
+    set --local indexes ($__fish_config_dir/functions/fish_history_abbreviation $argv)
     if test -z "$indexes"
         return 1
     end
@@ -12,4 +12,3 @@ function history_abbreviation
         echo $args
     end
 end
-
